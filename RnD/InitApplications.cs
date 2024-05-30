5eb0d0880ee33c6e498947a66fe5933ee931488f
Christian Prior-Mamulyan <cprior@gmail.com>
2024-05-30 14:44:14 +0200

    Version bump with RnD coded workflow

---
+++ RnD/InitApplications.cs
@@ -0,0 +1,33 @@
+using LuckyLawrencium.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace LuckyLawrencium.RnD
+{
+    public class InitApplications : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+            var screen = uiAutomation.Open(Descriptors.Calculator.Standard);
+        }
+    }
+}
